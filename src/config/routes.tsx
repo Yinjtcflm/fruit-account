ce02469fd4a7d99c14c27d754f02c61053bbdbc6
Yinjtcflm <15297927693@163.com>
2023-02-21 20:46:30 +0800

    feature:创建欢迎界面的几个页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,7 +1,23 @@
+import { RouteRecordRaw } from "vue-router";
+import { First } from "../components/welcome/first";
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { Welcome } from "../views/Welcome";
+import { Second } from "../components/welcome/second";
+import { Third } from "../components/welcome/third";
+import { Fourth } from "../components/welcome/fourth";
 
-export const routes = [
+export const routes: RouteRecordRaw[] = [
   { path: "/", component: Foo },
   { path: "/about", component: Bar },
+  {
+    path: "/welcome",
+    component: Welcome,
+    children: [
+      { path: "one", component: First },
+      { path: "two", component: Second },
+      { path: "three", component: Third },
+      { path: "four", component: Fourth },
+    ],
+  },
 ];
