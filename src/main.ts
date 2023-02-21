898564534e188ed5d8df9c7ae576ac9fef156088
Yinjtcflm <15297927693@163.com>
2023-02-21 19:52:34 +0800

    引入vue-route

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import { createApp } from "vue";
 import { App } from "./App";
+import { Foo } from "./views/Foo";
+import { createRouter, createWebHashHistory } from "vue-router";
+import { Bar } from "./views/Bar";
+const routes = [
+  { path: "/", component: Foo },
+  { path: "/about", component: Bar },
+];
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
