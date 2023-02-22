d841d87d0ac2a096dec234dbbf38f8d48902ad45
Yinjtcflm <15297927693@163.com>
2023-02-22 21:47:17 +0800

    修改默认路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,19 +1,17 @@
 import { RouteRecordRaw } from "vue-router";
 import { First } from "../components/welcome/First";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
 import { Second } from "../components/welcome/Second";
 import { Third } from "../components/welcome/Third";
 import { Fourth } from "../components/welcome/Fourth";
 
 export const routes: RouteRecordRaw[] = [
-  { path: "/", component: Foo },
-  { path: "/about", component: Bar },
+  { path: "/", redirect: "/welcome" },
   {
     path: "/welcome",
     component: Welcome,
     children: [
+      { path: "", redirect: "/welcome/one" },
       { path: "one", component: First },
       { path: "two", component: Second },
       { path: "three", component: Third },
