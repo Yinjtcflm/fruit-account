b788d62985255cd707b96ae161b97664c3252e1d
Yinjtcflm <15297927693@163.com>
2023-03-02 17:41:41 +0800

    wip:/items/create 页面

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,13 +1,26 @@
-import { defineComponent,PropType } from 'vue'
-import s from './ItemCreate.module.scss';
-export const ItemCreate=defineComponent({
-props:{
-name:{
-type:String as PropType<string>
-}
-},
-   setup:(props,context)=>{
-     return ()=>(
-      <div></div>
-     )}
-})
+import { defineComponent, PropType } from "vue";
+import { MainLayout } from "../../layouts/MainLayout";
+import { Icon } from "../../shared/Icon";
+import s from "./ItemCreate.module.scss";
+export const ItemCreate = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<string>,
+    },
+  },
+  setup: (props, context) => {
+    return () => (
+      <MainLayout>
+        {{
+          title: () => "记一笔",
+          icon: () => <Icon name="left" class={s.navIcon} />,
+          default: () => (
+            <>
+              <div>main</div>
+            </>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
