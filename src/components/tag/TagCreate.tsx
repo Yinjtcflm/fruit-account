2301796e7c301e8f0e959df6394cce5376b3fbd3
Yinjtcflm <15297927693@163.com>
2023-03-10 20:35:20 +0800

    完成 TagEdit 相关样式

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,10 +1,8 @@
 import { defineComponent, PropType, reactive } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
-import s from "./Tag.module.scss";
 import { Icon } from "../../shared/Icon";
-import { Button } from "../../shared/Button";
-import { EmojiSelect } from "../../shared/EmojiSelect";
 import { Rules, validate } from "../../shared/validate";
+import { TagForm } from "./TagForm";
 export const TagCreate = defineComponent({
   props: {
     name: {
@@ -33,10 +31,6 @@ export const TagCreate = defineComponent({
         sign: undefined,
       });
       Object.assign(errors, validate(formData, rules));
-      // errors = {
-      //   name: ["错误1", "错误2"],
-      //   sign: ["错误3", "错误4"],
-      // };
       e.preventDefault();
     };
     return () => (
@@ -44,44 +38,7 @@ export const TagCreate = defineComponent({
         {{
           title: () => "新建标签",
           icon: () => <Icon name="left" />,
-          default: () => (
-            <form class={s.form} onSubmit={onSubmit}>
-              <div class={s.formRow}>
-                <label class={s.formLabel}>
-                  <span class={s.formItem_name}>标签名</span>
-                  <div class={s.formItem_value}>
-                    <input
-                      v-model={formData.name}
-                      class={[s.formItem, s.input, s.error]}
-                    ></input>
-                  </div>
-                  <div class={s.formItem_errorHint}>
-                    <span>{errors["name"] ? errors["name"][0] : "　"}</span>
-                  </div>
-                </label>
-              </div>
-              <div class={s.formRow}>
-                <label class={s.formLabel}>
-                  <span class={s.formItem_name}>符号{formData.sign}</span>
-                  <div class={s.formItem_value}>
-                    <EmojiSelect
-                      v-model={formData.sign}
-                      class={[s.formItem, s.emojiList, s.error]}
-                    />
-                  </div>
-                  <div class={s.formItem_errorHint}>
-                    <span>{errors["sign"] ? errors["sign"][0] : "　"}</span>
-                  </div>
-                </label>
-              </div>
-              <p class={s.tips}>记账时长按标签即可编辑</p>
-              <div class={s.formRow}>
-                <div class={s.formItem_value}>
-                  <Button class={[s.formItem, s.button]}>确定</Button>
-                </div>
-              </div>
-            </form>
-          ),
+          default: () => <TagForm />,
         }}
       </MainLayout>
     );
