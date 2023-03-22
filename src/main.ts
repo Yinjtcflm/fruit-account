b249c0fc6bcee4c91137a012be59962aac5fa118
Yinjtcflm <15297927693@163.com>
2023-03-22 22:35:35 +0800

    优化代码，逻辑更加清晰

--- src/main.ts
+++ src/main.ts
@@ -9,21 +9,26 @@ import { fetchMe, mePromise } from "./shared/me";
 const router = createRouter({ history, routes });
 
 fetchMe();
+const whiteList: Record<string, "exact" | "stratsWith"> = {
+  "/": "exact",
+  "/start": "exact",
+  "/welcome": "stratsWith",
+  "/sign_in": "stratsWith",
+};
 router.beforeEach(async (to, from) => {
-  if (
-    to.path === "/" ||
-    to.path.startsWith("/welcome") ||
-    to.path.startsWith("/sign_in") ||
-    to.path === "/start"
-  ) {
-    return true;
-  } else {
-    const path = await mePromise!.then(
-      () => true,
-      () => "/sign_in?return_to=" + to.path
-    );
-    return path;
+  for (const key in whiteList) {
+    const value = whiteList[key];
+    if (value === "exact" && to.path === key) {
+      return true;
+    }
+    if (value === "stratsWith" && to.path.startsWith(key)) {
+      return true;
+    }
   }
+  return mePromise!.then(
+    () => true,
+    () => "/sign_in?return_to=" + to.path
+  );
 });
 const app = createApp(App);
 app.use(router);
