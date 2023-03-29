a424b2ebc21392c7e539c46bb0ac5f60ea4e4f9f
Yinjtcflm <15297927693@163.com>
2023-03-29 22:17:43 +0800

    初步转换折线图数据

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,11 +1,13 @@
-import { defineComponent, PropType, ref } from "vue";
+import { defineComponent, PropType, ref, computed, onMounted } from "vue";
 import { FormItem } from "../../shared/Form";
+import { http } from "../../shared/Http";
 import { Bars } from "./Bars";
 import s from "./Charts.module.scss";
 
 import { LineChart } from "./LineChart";
 import { PieChart } from "./PieChart";
-
+type Data1Item = { happen_at: string; amount: number };
+type Data1 = Data1Item[];
 export const Charts = defineComponent({
   props: {
     startDate: {
@@ -18,8 +20,25 @@ export const Charts = defineComponent({
     },
   },
   setup: (props, context) => {
-    const category = ref("expenses");
-
+    const kind = ref("expenses");
+    const data1 = ref<Data1>([]);
+    const betterData1 = computed(() => {
+      return data1.value.map(
+        (item) => [item.happen_at, item.amount] as [string, number]
+      );
+    });
+    onMounted(async () => {
+      const response = await http.get<{ groups: Data1; summary: number }>(
+        "/items/summary",
+        {
+          happen_after: props.startDate,
+          happen_before: props.endDate,
+          kind: kind.value,
+          _mock: "itemSummary",
+        }
+      );
+      data1.value = response.data.groups;
+    });
     return () => (
       <div class={s.wrapper}>
         <FormItem
@@ -29,9 +48,9 @@ export const Charts = defineComponent({
             { value: "expenses", text: "支出" },
             { value: "income", text: "收入" },
           ]}
-          v-model={category.value}
+          v-model={kind.value}
         />
-        <LineChart />
+        <LineChart data={betterData1.value} />
         <PieChart />
         <Bars />
       </div>
